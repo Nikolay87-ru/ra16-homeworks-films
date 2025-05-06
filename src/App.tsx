d54225ac7610c8d6e1754b748cec484f9579459d
Neto test <nikolajeliseev834@gmail.com>
2025-05-06 19:02:04 +0300

    Инициализация проекта

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useCards } from './components/Cards';
+
+export const App = () => {
+  const { cards } = useCards();
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+
+    </div>
+  );
+};
